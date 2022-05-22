f7868fe7565bdcf4acab3c6d71f46f29701d3604
l3x3d <56190805+l3x3d@users.noreply.github.com>
2022-05-21 23:53:44 -0700

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,7 @@
 import React from "react";
 
 export default function Header() {
-  const [navbarOpen, setNavbarOpen] = React.useState(false);
-  const [flyer, setFlyer] = React.useState(false);
-  const [flyerTwo, setFlyerTwo] = React.useState(false);
+
 
   return (
     <header class="fixed top-0 w-full clearNav z-50">
@@ -12,134 +10,14 @@ export default function Header() {
           <a
             href="/"
             class="flex text-3xl text-white font-medium mb-4 md:mb-0"
-          >NINE4
+          >smowlmfers
           </a>
-          <button
-            className="text-white pb-4 cursor-pointer leading-none px-3 py-1 md:hidden outline-none focus:outline-none content-end ml-auto"
-            type="button"
-            aria-label="button"
-            onClick={() => setNavbarOpen(!navbarOpen)}
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="white"
-              strokeWidth="2"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              className="feather feather-menu"
-            >
-              <line x1="3" y1="12" x2="21" y2="12"></line>
-              <line x1="3" y1="6" x2="21" y2="6"></line>
-              <line x1="3" y1="18" x2="21" y2="18"></line>
-            </svg>
-          </button>
-        </div>
-        <div
-          className={
-            "md:flex flex-grow items-center" +
-            (navbarOpen ? " flex" : " hidden")
-          }
-        >
-          <div class="md:ml-auto md:mr-auto font-4 pt-1 md:pl-14 pl-1 flex flex-wrap items-center md:text-base text-1xl md:justify-center justify-items-start">
-            <a class="mr-11 pr-2 cursor-pointer text-gray-300 hover:text-white font-semibold tr04">
-              Features
-            </a>
-            <div className="relative">
-              <button
-                type="button"
-                className="
-                   group rounded-md text-gray-300 inline-flex items-center text-base font-medium focus:outline-none pb-8'
-                  "
-                onMouseEnter={() => (setFlyer(!flyer), setFlyerTwo(false))}
-              >
-                <span className="tr04">Templates</span>
-                <svg
-                  className={
-                    flyer === true
-                      ? "transform rotate-180 ml-3 h-5 w-5 transition ease-out duration-200"
-                      : "ml-2 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-                  }
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </button>
-              <div
-                onMouseLeave={() => setFlyer(false)}
-                className={
-                  flyer
-                    ? "opacity-100 translate-y-0 transition ease-out duration-200 absolute z-10 -ml-4 mt-3 g327 border transform px-2 w-screen max-w-sm sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2"
-                    : "hidden opacity-0 translate-y-1 absolute z-10 -ml-4 mt-3 transform px-2 w-screen max-w-md sm:px-0 lg:ml-0 lg:left-1/2 lg:-translate-x-1/2"
-                }
-              >
-                <div className="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
-                  <div className="relative grid gap-6 bg-black px-2 py-6 sm:gap-8 ">
-                    <a
-                      href="/"
-                      className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-800 tr04"
-                    >
-                      <div className="ml-4">
-                        <p className="text-base font-medium text-white">
-                          NINE4 TEMPLATE #1
-                        </p>
-                        <p className="mt-1 text-sm text-gray-500">
-                          First Template
-                        </p>
-                      </div>
-                    </a>
-                    <a
-                      href="/"
-                      className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-800 tr04"
-                    >
-                      <div className="ml-4">
-                        <p className="text-base font-medium text-white">
-                          NINE4 TEMPLATE #2
-                        </p>
-                        <p className="mt-1 text-sm text-gray-500">
-                          Second Template
-                        </p>
-                      </div>
-                    </a>
-                    <a
-                      href="/"
-                      className="-m-3 p-3 flex items-start rounded-lg hover:bg-gray-800 tr04"
-                    >
-                      <div className="ml-4">
-                        <p className="text-base font-medium text-white">
-                          NINE4 TEMPLATE #3
-                        </p>
-                        <p className="mt-1 text-sm text-gray-500">
-                          Third Template
-                        </p>
-                      </div>
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <a class="mr-12 md:ml-11 ml-0 cursor-pointer text-gray-300 hover:text-white font-semibold tr04">
-              Pricing
-            </a>
-            <a class="mr-5 cursor-pointer text-gray-300 hover:text-white font-semibold tr04">
-              Careers
-            </a>
-          </div>
-          <a
-            href="https://twitter.com/"
+
+          <a className="flex flex-row items-right justify-between p-3 md:p-1"
+            href="https://twitter.com/smowlmfers"
             rel="noopener noreferrer"
             target="_blank"
-            className="invisible md:visible"
+
           >
             <svg
               title="Twitter"
@@ -158,26 +36,26 @@ export default function Header() {
           </a>
           <a
             data-v-54e46119=""
-            href="https://github.com/"
+            href="https://discord.com/invite/bnfm62QK"
             rel="noopener noreferrer"
             target="_blank"
-            className="pl-7 invisible md:visible"
+
           >
             <svg
               data-v-54e46119=""
-              width="30"
-              height="20"
-              viewBox="0 0 25 24"
+              width="40"
+              height="40"
+              viewBox="0 0 30 16"
               fill="none"
               xmlns="http://www.w3.org/2000/svg"
-              title="GitHub logo"
-              class="github-link--logo"
+              title="Discord Logo"
+              class="discord-link--logo"
             >
               <path
                 data-v-54e46119=""
                 fill-rule="evenodd"
                 clip-rule="evenodd"
-                d="M12.3019 0C5.50526 0 0 5.50526 0 12.3019C0 17.7392 3.52669 22.3458 8.4127 23.977C9.0244 24.0902 9.25095 23.7126 9.25095 23.3804C9.25095 23.0858 9.2434 22.3156 9.23585 21.2885C5.81488 22.0286 5.08991 19.6422 5.08991 19.6422C4.53108 18.2225 3.72304 17.8373 3.72304 17.8373C2.60537 17.0746 3.80611 17.0897 3.80611 17.0897C5.03705 17.1803 5.69405 18.3584 5.69405 18.3584C6.78906 20.2388 8.57129 19.6951 9.27361 19.3779C9.38688 18.585 9.70406 18.0412 10.0514 17.7316C7.32524 17.4295 4.45556 16.3723 4.45556 11.66C4.45556 10.3158 4.93132 9.22074 5.72426 8.35984C5.59588 8.04266 5.17298 6.79662 5.83754 5.10501C5.83754 5.10501 6.87213 4.77274 9.22074 6.36616C10.2025 6.0943 11.2522 5.95837 12.3019 5.95082C13.344 5.95837 14.4013 6.0943 15.383 6.36616C17.7316 4.77274 18.7662 5.10501 18.7662 5.10501C19.4383 6.79662 19.0154 8.05021 18.887 8.35984C19.6724 9.22074 20.1482 10.3158 20.1482 11.66C20.1482 16.3874 17.271 17.422 14.5297 17.7316C14.9677 18.1092 15.3679 18.8644 15.3679 20.0123C15.3679 21.6586 15.3528 22.9801 15.3528 23.3879C15.3528 23.7202 15.5718 24.0978 16.1986 23.977C21.0846 22.3458 24.6038 17.7392 24.6038 12.3094C24.6038 5.50526 19.0985 0 12.3019 0Z"
+                d="M13.545 2.907a13.227 13.227 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.19 12.19 0 0 0-3.658 0 8.258 8.258 0 0 0-.412-.833.051.051 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.041.041 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032c.001.014.01.028.021.037a13.276 13.276 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019c.308-.42.582-.863.818-1.329a.05.05 0 0 0-.01-.059.051.051 0 0 0-.018-.011 8.875 8.875 0 0 1-1.248-.595.05.05 0 0 1-.02-.066.051.051 0 0 1 .015-.019c.084-.063.168-.129.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.052.052 0 0 1 .053.007c.08.066.164.132.248.195a.051.051 0 0 1-.004.085 8.254 8.254 0 0 1-1.249.594.05.05 0 0 0-.03.03.052.052 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.235 13.235 0 0 0 4.001-2.02.049.049 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.034.034 0 0 0-.02-.019Zm-8.198 7.307c-.789 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612Zm5.316 0c-.788 0-1.438-.724-1.438-1.612 0-.889.637-1.613 1.438-1.613.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612Z"
                 fill="white"
               ></path>
             </svg>
